cf45c1649379d089c19439bf40cac89b9f090c5c
srikarb54 <55841163+srikarb54@users.noreply.github.com>
2020-12-08 22:09:38 +0530

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setTitle(`${message.client.user.username} Help`)
+      .setDescription("__**Take a look at the commands i have to play music**__")
+      .setColor("RED")
+      .setTimestamp();
+      
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
---
+++ commands/invite.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  description: "To add/invite the bot to your server",
+  execute(message, args) {
+    //set the permissions id here (https://discordapi.com/permissions.html)
+    var permissions = 70282305;
+
+    let invite = new MessageEmbed()
+      .setTitle(`**Interested by the functions of our bot**`)
+      .setDescription(
+        `**Then what are you waiting for? Invite me today!** \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=785806545832509470&permissions=${permissions}&scope=bot)`
+      )
+      .setURL(
+        `https://discord.com/oauth2/authorize?client_id=785806545832509470&permissions=${permissions}&scope=bot`
+      )
+      .setColor("RANDOM");
+    return message.channel.send(invite);
+  }
+};
